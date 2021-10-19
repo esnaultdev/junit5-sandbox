21d1fef6058b461846efa516e7bcf6a2bcaf4cf1
Matthieu Esnault <matthieu.esn0@gmail.com>
2021-10-20 00:08:28 +0900

    Introduce @DisplayName

--- app/src/test/java/dev/esnault/presentation/junit5/ColorTest.kt
+++ app/src/test/java/dev/esnault/presentation/junit5/ColorTest.kt
@@ -1,5 +1,6 @@
 package dev.esnault.presentation.junit5
 
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -9,24 +10,28 @@ import kotlin.test.assertEquals
 class ColorTest {
 
     @Nested
+    @DisplayName("toString()")
     inner class ToString {
 
         @Test
-        fun `toString should return #000000`() {
+        @DisplayName("should return #000000 for rgb(0, 0, 0)")
+        fun `#000000`() {
             val color = Color(0, 0, 0)
             val result = color.toString()
             assertEquals(expected = "#000000", actual = result)
         }
 
         @Test
-        fun `toString should return #FFFFFF`() {
+        @DisplayName("should return #FFFFFF for rgb(255, 255, 255)")
+        fun `#FFFFFF`() {
             val color = Color(255, 255, 255)
             val result = color.toString()
             assertEquals(expected = "#FFFFFF", actual = result)
         }
 
         @Test
-        fun `toString should return #123456`() {
+        @DisplayName("should return #123456 for rgb(18, 52, 86)")
+        fun `#123456`() {
             val color = Color(18, 52, 86)
             val result = color.toString()
             assertEquals(expected = "#123456", actual = result)
@@ -34,13 +39,15 @@ class ColorTest {
     }
 
     @Nested
+    @DisplayName("fromString()")
     inner class FromString {
 
         @Nested
         inner class Valid {
 
             @Test
-            fun `fromString should parse #000000`() {
+            @DisplayName("should parse #000000 as rgb(0, 0, 0)")
+            fun `#000000`() {
                 val colorString = "#000000"
                 val result = Color.fromString(colorString)
                 val expected = Color(0, 0, 0)
@@ -48,7 +55,8 @@ class ColorTest {
             }
 
             @Test
-            fun `fromString should parse #FFFFFF`() {
+            @DisplayName("should parse #FFFFFF as rgb(255, 255, 255)")
+            fun `#FFFFFF`() {
                 val colorString = "#FFFFFF"
                 val result = Color.fromString(colorString)
                 val expected = Color(255, 255, 255)
@@ -56,7 +64,8 @@ class ColorTest {
             }
 
             @Test
-            fun `fromString should parse #123456`() {
+            @DisplayName("should parse #123456 as rgb(18, 52, 86)")
+            fun `#123456`() {
                 val colorString = "#123456"
                 val result = Color.fromString(colorString)
                 val expected = Color(18, 52, 86)
@@ -68,31 +77,36 @@ class ColorTest {
         inner class Invalid {
 
             @Test
-            fun `fromString should throw for an empty string`() {
+            @DisplayName("should throw for an empty string")
+            fun `empty string`() {
                 val colorString = ""
                 assertThrows<IllegalArgumentException> { Color.fromString(colorString) }
             }
 
             @Test
-            fun `fromString should throw for an invalid format (no #)`() {
+            @DisplayName("should throw for an invalid format (no #)")
+            fun `invalid format (no #)`() {
                 val colorString = "FFFFFF"
                 assertThrows<IllegalArgumentException> { Color.fromString(colorString) }
             }
 
             @Test
-            fun `fromString should throw for an invalid format (too short)`() {
+            @DisplayName("should throw for an invalid format (too short)")
+            fun `invalid format (too short)`() {
                 val colorString = "#FFF"
                 assertThrows<IllegalArgumentException> { Color.fromString(colorString) }
             }
 
             @Test
-            fun `fromString should throw for an invalid format (too long)`() {
+            @DisplayName("should throw for an invalid format (too long)")
+            fun `invalid format (too long)`() {
                 val colorString = "#FFFFFFFF"
                 assertThrows<IllegalArgumentException> { Color.fromString(colorString) }
             }
 
             @Test
-            fun `fromString should throw for an invalid format (not hex)`() {
+            @DisplayName("should throw for an invalid format (not hex)")
+            fun `invalid format (not hex)`() {
                 val colorString = "#NOTHEX"
                 assertThrows<IllegalArgumentException> { Color.fromString(colorString) }
             }
